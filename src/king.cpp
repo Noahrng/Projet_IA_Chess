a722633570794d7c3215d17f8c5178a72ec05964
Valentin <valent66250@gmail.com>
2026-02-10 12:06:35 +0100

    coordinates & pawn,knight,king

---
+++ src/king.cpp
@@ -0,0 +1,15 @@
+#include <king.hpp>
+
+bool King::canMove(Coordinates coord)
+{
+    int distx=this->coords.distX(coord);
+    int disty=this->coords.distY(coord);
+
+    if((distx==1 || disty==1) && coords!=coord)
+    {
+        return true;
+    }
+
+    return false;
+}
+
--- src/pawn.cpp
+++ src/pawn.cpp
@@ -1,6 +1,37 @@
 #include <pawn.hpp>
 
-bool Pawn::canMove(Coordinates coord){
-    
+bool Pawn::canMove(Coordinates coord)
+{
+    int dist=this->coords.distY(coord);
+    if(color==false)
+    {
+        if(dist>=1 && dist<=2 && this->coords.distY(1)==0)
+        {
+            return true;
+        }
+        else if(dist == 1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    else
+    {
+        if(dist>=1 && dist<=2 && this->coords.distY(7)==0)
+        {
+            return true;
+        }
+        else if(dist == 1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
 
