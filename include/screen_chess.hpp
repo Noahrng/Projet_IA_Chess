414d8ec82f83ba4d65c25552273446648af154f4
Valentin <valent66250@gmail.com>
2026-03-10 12:09:26 +0100

    promotion graphique

--- include/screen_chess.hpp
+++ include/screen_chess.hpp
@@ -30,12 +30,14 @@ class ChessScreen : public Screen
         bool side;
         bool finished;
         std::map<AssetID,std::unique_ptr<GameAsset>> images;
+        Coordinates promoted;
+        bool color;
 
     public:
         ChessScreen(GameController&);
         ~ChessScreen();
 
-        void scrollPiece(Coordinates);
+        void scrollPiece(Coordinates,int);
         
         void drawAsset(AssetID,int,int,int,Color=WHITE);
         void drawPieces(int,Coordinates={-1,-1});
@@ -44,6 +46,7 @@ class ChessScreen : public Screen
         void addImage(AssetID,const std::string&);
 
         AssetID getAssetForPiece(const Piece&,bool);
+        AssetID getAssetForPiece(const PieceType&,bool);
         Coordinates getCoords(int);
 
         void switchSide();
