bf6eaaf46cadd14bf06b588a63e9400ac40a95a4
Valentin <valent66250@gmail.com>
2026-03-12 14:33:23 +0100

    opti evaluator

--- src/evaluator.cpp
+++ src/evaluator.cpp
@@ -7,19 +7,11 @@ Evaluator::Evaluator(GameController &game):game{game}, MAX_MATERIAL{39.0},MAX_MO
 
 double Evaluator::evaluateMaterial()
 {
-    double white_score=0.0;
-    double black_score=0.0;
-    
-
     Player &current = game.getCurrentPlayer();
     Player &waiting = game.getWaitingPlayer();
-    Player &white = current.isWhite() ? current : waiting;
-    Player &black = current.isBlack() ? current : waiting;
-    
-    white_score=white.getPoint();
-    black_score=black.getPoint();
 
-    return white_score-black_score;
+    return (current.isWhite() ? current.getPoint() : waiting.getPoint()) - (current.isBlack() ? current.getPoint() : waiting.getPoint());
+
 }
 
 double Evaluator::evaluate()
